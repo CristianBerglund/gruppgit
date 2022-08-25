0471155ebab00fc828580919dd03dcbae9ef398a
John Landtblom <john.landtblom@live.se>
2022-08-25 15:31:50 +0200

    added get request

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  headers = { "cache-control": "no-cache" };
+  body = { status: "available" };
+  res.status(200).json(body);
+});
+
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, console.log(`Im inside now!${PORT}`));
