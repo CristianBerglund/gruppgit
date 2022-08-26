e4e3a544eb3964ff2b7307378b183eb74a2c6e7d
Jergie Mascan <jiji.mascan@gmail.com>
2022-08-26 10:37:30 +0200

    added frontend

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
-
+const cors = require("cors");
+const path = require("path");
 const app = express();
+app.use(cors());
 
 app.get("/", (req, res) => {
   headers = { "cache-control": "no-cache" };
   body = { status: "available" };
   res.status(200).json(body);
 });
+
+app.use(express.static(path.join(__dirname, "frontend")));
 
 const PORT = process.env.PORT || 3001;
 
